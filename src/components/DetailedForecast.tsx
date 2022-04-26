f65ad2f80a4687554f5b3317375028bc644791e3
JoelMcMahon <80890808+JoelMcMahon@users.noreply.github.com>
2022-04-26 14:04:00 +0100

    add styling changes to detailed forecast

--- src/components/DetailedForecast.tsx
+++ src/components/DetailedForecast.tsx
@@ -8,20 +8,24 @@ interface IProps {
 
 const DetailedForecast: React.FC<IProps> = ({ value }) => {
   const { city } = useCityContext();
-  console.log(city.forecast);
+  console.log(city);
   console.log(value);
 
   let cityForecast = city.forecast[parseInt(value)];
 
+  console.log(cityForecast, "<<<< City Forecast");
+
   return (
     <Grid container spacing={2}>
       <Grid item xs={4}>
-        <Card>
-          <Typography>{city.name}</Typography>
+        <Card
+          sx={{ border: "none", backgroundColor: "#064663", color: "white" }}
+        >
+          <Typography align="center">{city.name}</Typography>
           <Typography>{cityForecast.day.avgtemp_c}</Typography>
           <Typography>
-            <img src={cityForecast.condition.icon} alt="" />
-            {cityForecast.condition.text}
+            <img src={cityForecast.day.condition.icon} alt="" />
+            {cityForecast.day.condition.text}
           </Typography>
         </Card>
       </Grid>
